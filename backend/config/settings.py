bc478a5282f1596e19acf36aa6fd22dceb6b774a
yashthipsay <yashthpsay@gmail.com>
2026-01-29 13:54:56 +0000

    adding prod db

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -14,6 +14,8 @@ import os
 from pathlib import Path
 from datetime import timedelta
 
+from dj_database_url import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -111,16 +113,36 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "clinic",
+#         "USER": "clinic_user",
+#         "PASSWORD": "clinic_pass",
+#         "HOST": "db",
+#         "PORT": 5432,
+#     }
+# }
+
+
+
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "clinic",
-        "USER": "clinic_user",
-        "PASSWORD": "clinic_pass",
-        "HOST": "db",
-        "PORT": 5432,
+        'NAME': 'postgres_healthcare',
     }
 }
+
+DATABASE_URL = config("DATABASE_URL", cast=str, default="")
+if DATABASE_URL:
+    if DATABASE_URL.startswith("postgres://") or DATABASE_URL.startswith("postgresql://"):
+        DATABASES = {
+            "default": config(
+                default=DATABASE_URL,
+            )
+        }
+
 
 
 # Password validation
