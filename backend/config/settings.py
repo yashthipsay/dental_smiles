0c7f42563ae0836bdc61a423a4b165c23422679e
yashthipsay <yashthpsay@gmail.com>
2026-01-29 07:00:35 +0000

    deployment phase starts

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -156,7 +156,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 
