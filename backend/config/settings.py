5a82f67cd7cc5ab2c28c9f0e3ee982d2b29e08b4
yashthipsay <yashthpsay@gmail.com>
2026-01-29 13:28:57 +0000

    settings change debug mode

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-($=^jm$t*^7v8y084y8*ifzhdz2xxyydpy_m8yozccg@&g#b!h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     ".railway.app",
