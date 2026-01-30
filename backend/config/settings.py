b05edcf5f25747aa1b01ed71e6e953ffa3b9a006
yashthipsay <yashthpsay@gmail.com>
2026-01-30 17:41:03 +0000

    added whitenoise middleware

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -107,6 +107,7 @@ TWILIO_PHONE_NUMBER = "+12316384477"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -215,6 +216,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+STATICFILES_STORAGE = (
+    "whitenoise.storage.CompressedManifestStaticFilesStorage"
+)
 
 
 
